b2c5e66812e7a4c9d4fe9234cf774ae60fa281ec
mkrtashot <mkrtchyan.ashot@yahoo.com>
2021-12-05 16:24:25 +0400

    added 6th task

---
+++ 6.js
@@ -0,0 +1,39 @@
+"use strict";
+// Write a function which receives two strings and removes appearances of the second string from the
+// first one.
+
+function remover(str1, str2) {
+  arrStr1 = [];
+  arrStr2 = [];
+  arrStr1 = str1.split("");
+  arrStr2 = str2.split("");
+
+  let check = 0;
+
+  for (let i = 0; i < arrStr1.length; i++) {
+    for (let j = 0; j < arrStr2.length; j++) {
+      if (arrStr1[i] === arrStr2[j]) {
+        check++;
+      }
+
+      if (check === arrStr2.length) {
+        for (let d = arrStr2.length; d > 0; d--) {
+          arrStr1[i - d + 1] = "_";
+        }
+        check = 0;
+      }
+    }
+  }
+  str1 = arrStr1.join("");
+  arrStr1 = str1.split("_");
+  str1 = arrStr1.join("");
+  return str1;
+}
+
+console.log(
+  remover(
+    `Yes, London. You know: fish, chips, cup ‘o tea, bad
+    food, worse weather`,
+    "o"
+  )
+);
